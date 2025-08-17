456d32c3960d0997925e845a87636fcf9ba1f18a
María Paz Allendes Álvarez <mariapazallendes724@gmail.com>
2025-08-17 15:10:46 -0400

    actualizando

--- Tareas/T1/dccasillas.py
+++ Tareas/T1/dccasillas.py
@@ -2,7 +2,10 @@
 class DCCasillas:
 
     def __init__(self, usuario:str, config:str) -> None:
-        pass
+        self.usuario = usuario
+        self.puntaje = 0
+        self.tablero_actual = None
+        self.tableros = config
 
     def abrir_tablero(self, num_tablero:int) -> None:
         pass
